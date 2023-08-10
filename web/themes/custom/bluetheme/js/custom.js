6c7e5d6d97cfd07cc69548cc9a3607b0e423dc65
Akisargar <sargar.akshay96@gmail.com>
2023-08-10 16:01:59 +0530

    updated banner.

--- web/themes/custom/bluetheme/js/custom.js
+++ web/themes/custom/bluetheme/js/custom.js
@@ -1,6 +1,6 @@
-(function ($, Drupal) {
-    $(document).ready(function () {
-        $(window).scroll(function () {
+(function($, Drupal) {
+    $(document).ready(function() {
+        $(window).scroll(function() {
             if ($(this).scrollTop() > 50) {
                 $('#back-to-top').fadeIn();
             } else {
@@ -8,7 +8,7 @@
             }
         });
         // scroll body to 0px on click
-        $('#back-to-top').click(function () {
+        $('#back-to-top').click(function() {
             $('body,html').animate({
                 scrollTop: 0
             }, 400);
@@ -16,20 +16,23 @@
         });
     });
 
-    Drupal.behaviors.navigation = {
-      attach: function (context, settings) {
-          if(document.querySelectorAll('  [data-mobilenavbtn]').length > 0){
-            const mobileNavBtn = document.querySelectorAll('[data-mobilenavbtn]');
-            const mobileNav = document.querySelector('[data-mobilenav]');
-        
-            mobileNavBtn.forEach(item => {
-            item.addEventListener('click', (e) => {
-                e.preventDefault();
-                mobileNav.classList.toggle('opened');
-             })
-          })    
+    'use strict';
+    var slider_flag = 0;
+
+
+    Drupal.behaviors.slider = {
+        attach(context, settings) {
+            if (slider_flag == 0) {
+                const slider_ac = tns({
+                    container: '.my-slider',
+                    items: 1,
+                    slideBy: 'page',
+                    controls: true,
+                    nav: false,
+                    loop: true,
+                });
+                slider_flag = 1;
+            }
         }
-    
-      }
-  }
+    };
 })(jQuery, Drupal);
